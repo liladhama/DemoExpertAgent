f438d288b6e422c917e30b7837864eb4e40cec1e
liladhama <samadhidas108@gmail.com>
2025-06-21 17:29:20 +0400

    revert: убран внешний контейнер, возврат к базовой версии App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,8 @@ import DrevsOnboardingDialog from './components/DrevsOnboardingDialog';
 
 function App() {
   return (
-    <div style={{ height: '100vh', width: '100vw', overflow: 'hidden', display: 'flex', flexDirection: 'column', position: 'relative' }}>
-      <div className="App">
-        <DrevsOnboardingDialog />
-      </div>
+    <div className="App">
+      <DrevsOnboardingDialog />
     </div>
   );
 }
